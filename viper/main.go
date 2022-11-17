a4ceef24be54aeca1f02a0a45d8a12a61f776912
César Caycho <cfabrica46@gmail.com>
2022-11-16 23:47:33 -0500

    feat: completed viper

--- viper/main.go
+++ viper/main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type configType struct {
+	InitParam InitParam `json:"initParam"`
+	// TagLib    TagLib    `json:"tagLib"`
+}
+
+type InitParam struct {
+	InstallAt  string `json:"installAt"`
+	Version    string `json:"version"`
+	StaticPath string `json:"staticPath"`
+}
+
+type TagLib struct {
+	TagLibASD string `json:"tagLibASD"`
+	TagLibLoc string `json:"taglibLoc"`
+}
 
 func main() {
-	fmt.Println("vim-go")
+	log.SetFlags(log.Lshortfile)
+
+	var config configType
+	var s string
+
+	viper.SetConfigType("json")
+	viper.SetConfigFile("./testJSONConfig.json")
+	fmt.Printf("Using config: %s\n", viper.ConfigFileUsed())
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal(err)
+	}
+
+	if viper.IsSet("init-param.installAt") {
+		fmt.Println("init-param.installAt:", viper.Get("init-param.installAt"))
+	} else {
+		fmt.Println("init-param.installAt not set.")
+	}
+
+	if viper.IsSet("init-param.staticPath") {
+		fmt.Println("init-param.staticPath:", viper.Get("init-param.staticPath"))
+	} else {
+		fmt.Println("init-param.staticPath is not set.")
+	}
+
+	if err := viper.Unmarshal(&config); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Config Loaded:", config)
+
+	if err := viper.UnmarshalKey("init-param.installAt", &s); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("key Loaded:", s)
 }
